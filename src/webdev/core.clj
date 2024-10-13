8972e35e63f8ab00c8bed1cb32850008453b78db
Ryan B <ryan.brilliant.nirwana@gmail.com>
2024-10-13 13:01:35 +0700

    about handledump yo calc

--- src/webdev/core.clj
+++ src/webdev/core.clj
@@ -2,7 +2,8 @@
   (:require [ring.adapter.jetty :as jetty]
             [ring.middleware.reload :refer [wrap-reload]]
             [compojure.core :refer [defroutes GET]]
-            [compojure.route :refer [not-found]]))
+            [compojure.route :refer [not-found]]
+            [ring.handler.dump :refer [handle-dump]]))
 
 (defn goodbye [_]
   {:status 200
@@ -14,9 +15,43 @@
    :body "Hello, World!!!!"
    :headers {}})
 
+(defn about [_]
+  {:status 200
+   :body "Hello, i'm a hobbyist programmer that somehow found lisp to make website."
+   :headers {}})
+
+(defn yo [req]
+  (let [name (get-in req [:route-params :name])]
+    {:status 200
+     :body (str "Yo! " name "!")
+     :headers {}}))
+
+(def ops
+  {"+" +
+   "-" -
+   "*" *
+   ":" /})
+
+(defn calc [req]
+  (let [first_num (Integer. (get-in req [:route-params :first_num]))
+        second_num (Integer. (get-in req [:route-params :second_num]))
+        operator (get-in req [:route-params :operator])
+        f (get ops operator)]
+    (if f
+      {:status 200
+       :body (str first_num operator second_num "=" (f first_num second_num))
+       :headers {}}
+      {:status 404
+       :body (str "Unknown operator: " operator)
+       :headers {}})))
+
 (defroutes app
   (GET "/" [] greet)
   (GET "/goodbye" [] goodbye)
+  (GET "/about" [] about)
+  (GET "/request" [] handle-dump)
+  (GET "/yo/:name" [] yo)
+  (GET "/calc/:first_num/:operator/:second_num" [] calc)
   (not-found "Page not found."))
 
 (defn -main [port]
